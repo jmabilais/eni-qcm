be2cae33ea424699b0e8cfadedbc84c1cc357b87
pierrev <a3ka@live.fr>
2014-04-01 17:04:59 +0200

    [BackendBundle] ajout vue accueil administration

--- src/Eni/BackendBundle/Controller/DefaultController.php
+++ src/Eni/BackendBundle/Controller/DefaultController.php
@@ -9,11 +9,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/frontend/hello/{name}")
+     * @Route("/administration")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        return array();
     }
 }
